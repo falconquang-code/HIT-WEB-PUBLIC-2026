11b98cdb8f776852d1da884142133feb757496a2
falconquang@gmail.com <=>
2026-09-06 09:52:50 +0700

    New

---
+++ jv.js
@@ -0,0 +1,5 @@
+const container= document.getElementsByClassName("tittle");
+console.log(container)
+const head1=document.createElement("h1");
+head1.textContent="Than Manh Quang"
+container[0].appendChild(head1)
